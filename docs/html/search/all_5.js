deaf7d6433b49d176d161c85e78daf8c195ece89
Igor Dedkovskiy <divmy@yandex.ru>
2022-10-11 20:06:56 +0000

    Generate docs

---
+++ docs/html/search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['input_2ecpp_18',['input.cpp',['../input_8cpp.html',1,'']]],
+  ['input_2ehpp_19',['input.hpp',['../input_8hpp.html',1,'']]],
+  ['input_5ft_20',['input_t',['../input_8hpp.html#a56f4c879318f7dd18b12c735d67aaa80',1,'input.hpp']]]
+];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['otus_20homework_20_237_20_2d_20bayan_100',['otus homework #7 - bayan',['../md__r_e_a_d_m_e.html',1,'']]]
+];
